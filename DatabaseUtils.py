cbf4f3249b184acf96dde3ac4ddc3813e487993b
DartDivora <nsanft@sanft.com>
2017-12-19 15:30:24 -0600

    Added the ability to add rows to any SQL table
    1) Updated DatabaseUtils with methods to execute any query, as well as a
    method to build an HTML Form based off of a table and method.
    2) Updated FlaskExample to dynamically route requests with methods (like
    accounts/add to add).

--- DatabaseUtils.py
+++ DatabaseUtils.py
@@ -83,3 +83,30 @@ def selectAllFromTableHTML(tableName):
         resultHTML += "</tr> \n"
     resultHTML += "</tbody></table></div></body></html>"
     return resultHTML
+
+def executeQuery(query,tupleValues):
+    cur = con.cursor()
+    cur.execute(query,tupleValues)
+    con.commit()
+
+def getHTMLForm(tableName,methodName):
+    cur = con.cursor()
+    query = """PRAGMA table_info('{0}')""".format(tableName,)
+    cur.execute(query)
+    formHTML = """<html lang="en"><head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="hi">
+        <meta name="author" content="">
+        <title>test.com</title>
+        <link href="/static/styles/bootstrap.min.css" rel="stylesheet">
+        </head>
+    <body>
+    <div class="container">
+        <h1>Enter some text</h1>
+        <form action="/{0}/{1}" method="POST">""".format(str(tableName),str(methodName))
+    for row in cur:
+        formHTML += """<label for="{0}">{0}({1}):</label><input type="text" class="form-control" id="{0}" name="{0}">""".format(str(row[1]),str(row[2]))
+    formHTML += """<input type="submit" name="form" value="Submit"></form></div></body></html>"""
+    return formHTML
--- FlaskExample.py
+++ FlaskExample.py
@@ -12,12 +12,38 @@ def index():
     return app.send_static_file("index.html")
 
 
+@app.route("/<tableName>/<methodName>", methods=['GET'])
+def getForm(tableName, methodName):
+    if(methodName.upper() == "ADD"):
+        return du.getHTMLForm(tableName, methodName)
+    else:
+        return "Did not recognize method: " + str(methodName)
+
+
+@app.route("/<tableName>/<methodName>", methods=['POST'])
+def insertNewRow(tableName, methodName):
+    columnNames = []
+    columnParameters = []
+    columnValues = []
+    for k, v in request.form.items():
+        if k.upper() != "FORM":
+            columnNames.append(k)
+            columnParameters.append("?")
+            columnValues.append(v)
+    query = """INSERT INTO {0} ({1}) VALUES ({2})""".format(
+        tableName, ",".join(columnNames), ",".join(columnParameters))
+    du.executeQuery(query, tuple(columnValues))
+    result = "Successfully inserted row!"
+    return result
+
+
 @app.route("/<tableName>")
 def getSQLTable(tableName):
     if(du.tableExists(tableName)):
         result = du.selectAllFromTableHTML(tableName)
         return result
     return "No table found with the name: " + tableName
+
 
 if __name__ == "__main__":
     app.run()
