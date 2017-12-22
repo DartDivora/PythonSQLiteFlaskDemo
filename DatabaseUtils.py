48ad1f99272850f995600d5659893e96227fec48
DartDivora <nsanft@sanft.com>
2017-12-22 10:31:08 -0600

    You should have expected more refactoring
    1) Moved the generateExcelFile function to DatabaseUtils.
    2) Removed ExcelExample, as it was no longer needed.

--- DatabaseUtils.py
+++ DatabaseUtils.py
@@ -1,3 +1,4 @@
+from xlsxwriter.workbook import Workbook
 import sqlite3 as sql
 import Config
 
@@ -86,6 +87,28 @@ def getHTMLForm(tableName, methodName):
 
 
 """
+This function takes in a filepath and select SQL query and outputs the result (with columns) to the filepath specified.
+"""
+
+
+def generateExcelFile(path, selectQuery):
+    cur = con.cursor()
+    cur.execute(selectQuery)
+    workbook = Workbook(path)
+    sheet = workbook.add_worksheet()
+    columnNames = []
+    for column in cur.description:
+        columnNames.append(column[0])
+    listToWrite = cur.fetchall()
+    listToWrite.insert(0, tuple(columnNames))
+    print(listToWrite)
+    for r, row in enumerate(listToWrite):
+        for c, col in enumerate(row):
+            sheet.write(r, c, col)
+    workbook.close()
+
+
+"""
 This is the main database script used to communicate with the SQLite database.
 This contains all necessary methods to create the schema for this database.
 """
@@ -94,6 +117,7 @@ This contains all necessary methods to create the schema for this database.
 def main():
     createTables()
     # dropAllTables()
+    #generateExcelFile("test.xlsx","SELECT * FROM accounts")
 
 
 if __name__ == "__main__":
